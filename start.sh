e4abffa721cce16146318618d0db1f1de9b0cb47
langzhongyu <1837820987@qq.com>
2020-12-17 13:28:38 +0800

    'name'

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ git pull
 yarn --registry=https://registry.npm.taobao.org/ && yarn build
 
 #删除容器
-docker rm -f demo1 &> /dev/null
+docker rm -f vue-project &> /dev/null
 
 #启动容器
 docker run -d --restart=on-failure:5\
     -p 8081:80 \
     -v $PWD/dist:/usr/share/nginx/html \
-    --name demo1 nginx
+    --name vue-project nginx
